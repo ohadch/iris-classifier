95bd9de9f31187e185f81c915b7fcf2efe529da0
Ohad Chaet <ohadch9518@gmail.com>
2019-09-17 21:25:24 +0300

    Adds image uploader

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import ImageUploader from 'vue-image-upload-resize'
+Vue.use(ImageUploader);
 
 Vue.config.productionTip = false
 
--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <v-layout text-center align-center justify-center mt-2>
     <v-flex xs12 sm6 md6>
-    <ModelTraining />
+      <ImageUpload />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import ModelTraining from "@/components/ModelTraining";
+import ImageUpload from "@/components/ImageUpload";
 export default {
   components: {
-    ModelTraining
+    ImageUpload
   }
 };
 </script>
