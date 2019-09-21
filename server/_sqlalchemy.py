32ff286271de844e00e10ec7fd231c76138c3863
Ohad Chaet <ohadch9518@gmail.com>
2019-09-21 12:17:27 +0300

    Adds user model

--- server/_sqlalchemy.py
+++ server/_sqlalchemy.py
@@ -17,3 +17,14 @@ logger.info(f"App version: {VERSION}")
 
 db = SQLAlchemy(app)
 
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
